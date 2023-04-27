83b9e2e1e9041aefed0da40aa6cecd4611e52039
Roshiori <brzrokuniyon@gmail.com>
2023-04-27 23:35:00 +0900

    fix: fetch api method

--- src/components/textbox.tsx
+++ src/components/textbox.tsx
@@ -1,10 +1,13 @@
-import { Button, Input, Stack, withDefaultVariant } from "@chakra-ui/react";
+import { Button, Input, Stack, Text, withDefaultVariant } from "@chakra-ui/react";
 import React from "react";
 import Result from "./result";
+import { NextResponse } from "next/server";
 
 export const SbContext = React.createContext("");
 export const AuContext = React.createContext("");
 export const RmContext = React.createContext("");
+let validate: boolean = false;
+let display: string = "";
 
 const TextBox = () => {
   const [imeiNum, setImeiNum] = React.useState("");
@@ -12,16 +15,40 @@ const TextBox = () => {
   const [austate, setAustate] = React.useState("N/A");
   const [rmstate, setRmstate] = React.useState("N/A");
 
-  const submitIMEI = () => {
-    async() => {
-      const SubmittedIMEI: string = JSON.stringify(imeiNum);
-      await fetch ("/api/getrestrictionvalue", {
-        method: "POST", 
-        body: SubmittedIMEI
-      }).then      
-    } 
-    setSbstate("まる");
+  const submitIMEI = async () => {
+    const paramIMEI = {
+      imei: imeiNum,
+    };
+      fetch("/api/getrestrictionvalue", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(paramIMEI),
+      }).then((Response) => console.log(Response.json()))
+
+      /*const res = await fetch("/api/getrestrictionvalue", {
+        method: "GET",
+      }).then(
+        (res) => console.log(res.json())
+      )*/
   };
+
+  if (imeiNum.length < 15 || imeiNum.length > 15 ){
+    validate = false;   
+    display = ""; 
+  }
+
+  else if (imeiNum.length == 15){
+    if (isNaN(Number(imeiNum))) {
+      validate = false;
+      display = "";
+    }
+    else {
+      validate = true;
+      display = "none";
+    }
+  }
 
   return (
     <>
@@ -34,8 +61,9 @@ const TextBox = () => {
                 value={imeiNum}
                 onChange={(e) => setImeiNum(e.target.value)}
               />
-              <Button onClick={submitIMEI}>確認</Button>
-              <p>IMEI: {imeiNum}</p>
+              <Button onClick={submitIMEI} isDisabled = {validate == false}>確認</Button>
+              <Text>IMEI: {imeiNum}</Text>
+              <Text color = "red" display={display} >IMEIを15桁の半角数字で入力してください</Text>
               <Result />
             </Stack>
           </RmContext.Provider>
--- src/pages/api/getrestrictionvalue.ts
+++ src/pages/api/getrestrictionvalue.ts
@@ -3,35 +3,114 @@ import { executablePath } from "puppeteer-core";
 const chromium = require("chrome-aws-lambda");
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const browser = await puppeteer.launch({ channel: "chrome" });
+type Data = {
+  sbrestriction: number;
+  aurestriction: number;
+  rmrestriction: number;
+}[];
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const browser = await puppeteer.launch({
+    channel: "chrome",
+    headless: false,
+  });
   const page = await browser.newPage();
   try {
+    //SoftBank/Y!
     await page.goto("https://ct99.my.softbank.jp/WBF/icv");
-    const inputIMEI = await page.type('input[name="imei"]', req.body);
-    const submitIMEI = await Promise.all([
+    await page.type('input[name="imei"]', req.body);
+    await Promise.all([
       page.waitForNavigation({ waitUntil: "load" }),
       page.click('[name="ACT_TE001"]'),
     ]);
-    let respondResult: number = 0;
-    const [getTag] = await page.$x(
-      '/html/body/div/table/tr/td/font[contains(@size, "8")]'
+    let sbrespondResult: number = 0;
+    const [sbgetTag] = await page.$x(
+      "/html/body/div[1]/table[2]/tbody/tr[2]/td/font"
     );
 
-    const sbResult = await getTag.getProperty("textContent");
-
-    if (sbResult == '<font size="8" color="#666666">○</font>') {
-      respondResult = 1;
-    } else if (getTag == '<font size="8" color="#666666">○</font>') {
-      respondResult = 2;
-    } else if (getTag == '<font size="8" color="#666666">○</font>') {
-      respondResult = -1;
-    } else if (getTag == '<font size="8" color="#666666">○</font>') {
-      respondResult = 4;
+    const sbResult = await (
+      await sbgetTag.getProperty("textContent")
+    ).jsonValue();
+    console.log(sbResult);
+    if (sbResult == "⚪︎") {
+      sbrespondResult = 1;
+    } else if (sbResult == "▲") {
+      sbrespondResult = 2;
+    } else if (sbResult == "×") {
+      sbrespondResult = -1;
+    } else if (sbResult == "－") {
+      sbrespondResult = 3;
     }
-    console.log(respondResult);
+    console.log(sbrespondResult);
+
+    //au/UQ
+    await page.goto("https://my.au.com/cmn/WCV009001/WCE009001.hc");
+    await page.type('input[name="IMEI"]', req.body);
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "load" }),
+      page.click('[name="次へ"]'),
+    ]);
+    let aurespondResult: number = 0;
+    let [augetTag] = await page.$x(
+      "/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div/div"
+    );
+    if ([augetTag == null]) {
+      [augetTag] = await page.$x("/html/body/div[1]/div[2]/p");
+    }
+
+    const auResult = await (
+      await augetTag.getProperty("textContent")
+    ).jsonValue();
+    console.log(auResult);
+    if (auResult == "⚪︎") {
+      aurespondResult = 1;
+    } else if (auResult == "△") {
+      aurespondResult = 2;
+    } else if (auResult == "×") {
+      aurespondResult = -1;
+    } else if (auResult == "－") {
+      aurespondResult = 3;
+    } else {
+      aurespondResult = 3;
+    }
+    console.log(aurespondResult);
+
+    //Rakuten
+    await page.goto("https://network.mobile.rakuten.co.jp/restriction/");
+    await page.type('input[name="imei"]', req.body);
+    await Promise.all([
+      page.waitForSelector("#search-result"),
+      page.click("#search"),
+    ]);
+    let rmrespondResult: number = 0;
+    const [rmgetTag] = await page.$x(
+      "/html/body/div/div/section/div/main/div/ul/li[2]/dl/dd"
+    );
+
+    const rmResult = await (
+      await rmgetTag.getProperty("textContent")
+    ).jsonValue();
+    console.log(rmResult);
+    if (rmResult == "⚪︎") {
+      rmrespondResult = 1;
+    } else if (rmResult == "△") {
+      rmrespondResult = 2;
+    } else if (rmResult == "×") {
+      rmrespondResult = -1;
+    } else if (rmResult == "-") {
+      rmrespondResult = 3;
+    }
+    console.log(rmrespondResult);
+
+    res.status(200).json([
+      {
+        sbrestriction: sbrespondResult,
+        aurestriction: aurespondResult,
+        rmrestriction: rmrespondResult,
+      },
+    ]);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   } finally {
     await browser.close();
     res.status(200).end();
